f5185411b7d2d7935b4eb211589cb90c1969b22b
PhilW <roundcube@tehinterweb.co.uk>
2010-06-16 21:02:51 +0100

    fix typo

--- jquery.maskedinput.js
+++ jquery.maskedinput.js
@@ -4,7 +4,7 @@
 	Copyright (c) 2007-2009 Josh Bush (digitalbush.com)
 	Licensed under the MIT license (http://digitalbush.com/projects/masked-input-plugin/#license)
 	Version: 1.2.2 (03/09/2009 22:39:06)
-	Modified by: Phil Weir
+	Modified by: Philip Weir
 */
 (function($) {
 	var pasteEventName = ($.browser.msie ? 'paste' : 'input') + ".mask";
