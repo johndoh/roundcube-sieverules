2e7e2a615acc8c6d77aa186184d4e485d5001c07
PhilW <roundcube@tehinterweb.co.uk>
2010-10-02 16:44:52 +0100

    add Internationalized Domain Name (IDNA) support (r4009)

--- sieverules.php
+++ sieverules.php
@@ -1130,7 +1130,7 @@ class sieverules extends rcube_plugin
 			// try to connect to managesieve server and to fetch the script
 			$this->sieve = new rcube_sieve($_SESSION['username'],
 						$rcmail->decrypt($_SESSION['password']),
-						rcube_parse_host($rcmail->config->get('sieverules_host')),
+						idn_to_ascii(rcube_parse_host($rcmail->config->get('sieverules_host'))),
 						$rcmail->config->get('sieverules_port'), $rcmail->config->get('sieverules_auth_type', NULL),
 						$rcmail->config->get('sieverules_usetls'), $this->current_ruleset,
 						$this->home, $rcmail->config->get('sieverules_use_elsif', true));
